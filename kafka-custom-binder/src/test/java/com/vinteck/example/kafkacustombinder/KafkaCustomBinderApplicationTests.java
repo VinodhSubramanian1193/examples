56047d4dde3a2c717853c91264cba1b5f7408cc3
v0s01ls <v0s01ls@homeoffice.wal-mart.com>
2020-12-09 16:54:47 +0530

    Add code for kafka single cluster custom configuration.

---
+++ kafka-custom-binder/src/test/java/com/vinteck/example/kafkacustombinder/KafkaCustomBinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinteck.example.kafkacustombinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCustomBinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
